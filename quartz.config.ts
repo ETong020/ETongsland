a47972e0311337fae07f58ca1d9f7a31f72353c8
ETong020 <etongq020@gmail.com>
2024-07-02 00:02:04 +0800

    Quartz sync: Jul 2, 2024, 12:02 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -33,7 +33,7 @@ const config: QuartzConfig = {
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
           dark: "#20b2aa",
-          secondary: "#284b63",
+          secondary: "#20b2aa",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
         },
