e3c1aac36ab630efe7826d8b9fbd24cd4866cb6d
ETong020 <etongq020@gmail.com>
2024-07-20 16:11:40 +0800

    vault backup: 2024-07-20 16:11:40

--- quartz.config.ts
+++ quartz.config.ts
@@ -54,7 +54,7 @@ const config: QuartzConfig = {
     transformers: [
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
-        priority: ["filesystem", "frontmatter"],
+        priority: ["frontmatter", "filesystem"],
       }),
       Plugin.SyntaxHighlighting({
         theme: {
