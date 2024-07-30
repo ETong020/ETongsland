ccb393dcb9604290b7e8367e570452f5c94c299f
ETong020 <etongq020@gmail.com>
2024-07-30 23:01:23 +0800

    vault backup: 2024-07-30 23:01:23

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,6 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: 'ETong020/ETongsland',
+        // from data-repo-id
+        repoId: 'R_kgDOMPy-Bg',
+        // from data-category
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: 'DIC_kwDOMPy-Bs4ChRf2',
+      }
+    }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/ETong020",
@@ -21,21 +36,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.ContentMeta(),
     Component.TagList(),
   ],
-  afterBody: [
-  Component.Comments({
-    provider: 'giscus',
-    options: {
-      // from data-repo
-      repo: 'ETong020/ETongsland',
-      // from data-repo-id
-      repoId: 'R_kgDOMPy-Bg',
-      // from data-category
-      category: 'Announcements',
-      // from data-category-id
-      categoryId: 'DIC_kwDOMPy-Bs4ChRf2',
-    }
-  }),
-],
+
 
   left: [
     Component.PageTitle(),
