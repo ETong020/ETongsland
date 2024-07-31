e2f41d170fdc38b21815f37979239d171104acac
ETong020 <etongq020@gmail.com>
2024-07-31 17:49:44 +0800

    vault backup: 2024-07-31 17:49:44

--- quartz.config.ts
+++ quartz.config.ts
@@ -42,7 +42,7 @@ const config: QuartzConfig = {
           lightgray: "#fdf5e6",
           gray: "#646464",
           darkgray: "#d4d4d4",
-          dark: "#145b7d",
+          dark: "#224b8f",
           secondary: "#007d65",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
